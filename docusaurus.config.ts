69736efbb4da9ea6f4e33438d4ccba6f987347ff
d3kryption <d3kryption@gmail.com>
2024-12-01 15:02:41 +0000

    - Temp URL workaround

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   url: 'https://docs.narrativetools.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/narrative-documentation/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
