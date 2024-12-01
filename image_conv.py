897447186f835ad99497bf754ccf74aa01034485
d3kryption <d3kryption@gmail.com>
2024-12-01 01:46:48 +0000

    Updates

--- image_conv.py
+++ image_conv.py
@@ -23,7 +23,7 @@ def search_and_convert_images(folder_path, exclude_filenames=None):
                 convert_image_to_webp(file_path)
 
 if __name__ == "__main__":
-    folder_to_search = "/home/jackhannibalmarioripper/Documents/Websites/narrative-docs/static/img/quests-and-dialogue/ui/"  # Replace with your target folder path
+    folder_to_search = "/home/jackhannibalmarioripper/Documents/Websites/narrative-docs/static/img/navigator/"  # Replace with your target folder path
     excluded_files = ['android-chrome-192x192.webp', 'android-chrome-512x512.webp', 'apple-touch-icon.webp', 'docusaurus.webp', 'docusaurus-social-card.jpg', 'favicon.ico', 'favicon-16x16.webp', 'favicon-32x32.webp']  # Add any filenames you want to exclude
     search_and_convert_images(folder_to_search, exclude_filenames=excluded_files)
 
